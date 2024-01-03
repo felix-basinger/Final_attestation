be372b2902820f480e42042532b9a3624f1fbd3d
felix-basinger <fel_simon@mail.ru>
2024-01-03 16:41:47 +0100

    Added menu point "add animal" and file of app launch

--- final_project/menu/menu.py
+++ final_project/menu/menu.py
@@ -3,3 +3,15 @@ from final_project.animal_registry.animal_registry import AnimalRegistry
 
 def main_menu():
     animal_registry = AnimalRegistry()
+    while True:
+        print(
+            '\n1. Add Animal\n2. Remove Animal\n3. View Animals\n4. Register Animal\n'
+            '5. Teach Command\n6. Search by name or ID\n7. Search by Keyword\n8. Display Animal Type\n9. Exit')
+        choice = input('Enter your choice (1-9): ')
+
+        if choice == '1':
+            name = input('Enter animal name: ')
+            species = input('Enter animal species: ')
+            animal_registry.add_animal(name, species)
+        else:
+            print('Invalid choice. Please enter a number between 1 and 9.')
---
+++ final_project/start_app.py
@@ -0,0 +1,4 @@
+from final_project.menu.menu import main_menu
+
+if __name__ == "__main__":
+    main_menu()
